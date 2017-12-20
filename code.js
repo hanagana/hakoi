05a0cde40c80cb725907394948432790c34c8002
hanagana <hanguyen.iter@gmail.com>
2017-12-20 08:31:16 +0700

    go

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    alert("hello world");
+    console.log(getPrice());
+
+    function getPrice() {
+        return window.price;
+
+        // $.ajax({
+        //     url: "https://api.binance.com/api/v1/ticker/allPrices",
+        //     method: "GET"
+        // }).done(function(data, a, b) {
+        //     console.log(data);
+        //     console.log(a);
+        //     console.log(b);
+        // })
+    }
 });
